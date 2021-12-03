5a476cf83e874aa1d01de4b84c96fcddd827bcd0
Suez Dirie <suedirie@WKMZTC63B1E8.global.publicisgroupe.net>
2021-12-03 16:53:52 +0000

    solution for day 3b

--- 2021/SuezD/3/b.py
+++ 2021/SuezD/3/b.py
@@ -1,31 +1,44 @@
 import numpy as np
 
 def lifeSupport(filename):
-    data = np.genfromtxt(filename, delimiter = 1)
+    a = np.genfromtxt(filename, delimiter = 1)
+
+    i=0
+    cols = len(a)
+    col = a[:,i]
+
+    if sum(col) == cols/2 or sum(col) > int(cols/2):
+        criteria = 1
+    else:
+        criteria = 0
+        
+    generatorData = a[a[:,i]==criteria, :]
+    scrubberData = a[a[:,i]==int(not criteria), :]
+    
+    g = f(generatorData, 1)  
+    s = f(scrubberData, 0)
+    
+    return g*s
     
     
-    i = 0
+def f(data, c):
+    i = 1
     while(len(data) > 1):
         cols = len(data)
-        print(data)
         col = data[:,i]
         
         if sum(col) == cols/2 or sum(col) > int(cols/2):
-            criteria = 1
+            criteria = c
         else:
-            criteria = 0
+            criteria = int(not c)
             
-        print(col, int(cols/2), criteria)
-        print() 
         data = data[data[:,i]==criteria, :]
         i+=1
     
-    o2 = int(''.join([str(int(i)) for i in data[0]]),2)
-    print(o2)
-    
-    
-    
-        
+    x = (int(''.join([str(int(i)) for i in data[0]]),2))
+    return x
+
 
 if __name__=="__main__":
-    lifeSupport('test.txt')
+    assert lifeSupport('test.txt') == 230
+    print(lifeSupport('input.txt'))
