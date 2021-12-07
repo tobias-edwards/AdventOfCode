b30464cfd08184e3358de0e4dcdda68f652acb7d
tobias-edwards <tobiasrichardedwards@gmail.com>
2021-12-07 00:40:21 +0000

    chore(count): Use efficient param-reassigned reduce()

--- 2021/tobias-edwards/utils/array.js
+++ 2021/tobias-edwards/utils/array.js
@@ -1,12 +1,11 @@
-export const count = (arr) => {
-  const counts = {};
-  arr.forEach((item) => {
-    counts[item] = Object.prototype.hasOwnProperty.call(counts, item)
-      ? counts[item] + 1
+const count = (arr) =>
+  arr.reduce((counts, el) => {
+    /* eslint-disable-next-line no-param-reassign */
+    counts[el] = Object.prototype.hasOwnProperty.call(counts, el)
+      ? counts[el] + 1
       : 1;
-  });
-  return counts;
-};
+    return counts;
+  }, {});
 
 export const duplicates = (arr) => {
   const counts = count(arr);
